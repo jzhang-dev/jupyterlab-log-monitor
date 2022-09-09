5f8bc76091c6ad60ee99c85f82782b120feb66ca
Jia-Yuan Zhang <sloppybaron@gmail.com>
2022-09-09 12:45:53 +0000

    Test open blank page

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the jupyterlab-log-monitor extension.
@@ -14,7 +16,30 @@ import { ICommandPalette } from '@jupyterlab/apputils';
   requires: [ICommandPalette],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod is activated!');
-    console.log('ICommandPalette:', palette);
+  
+    // Create a blank content widget inside of a MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'apod-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+  
+    // Add an application command
+    const command: string = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+  
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
